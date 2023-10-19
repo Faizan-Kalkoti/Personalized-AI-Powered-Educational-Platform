cfb5aa1f5e6bca29b36e4e3886b54a86f7d6c14d
Faizan <faizan.kalkoti@gmail.com>
2023-10-19 08:12:58 +0530

    this is graph done sections

--- sections/models.py
+++ sections/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
 from accounts.models import Teacher, Student
 from courses.models import Course, Course_members
-
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -17,9 +19,24 @@ class Section(models.Model):
                           ('Hard','Hard'),
                           ('Expert','Expert')] 
     Difficulty = models.CharField(max_length=20, choices=Accessible_at_level, default='Easy')
+    slug = models.SlugField()
+
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.section_name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.section_name)
+            unique_slug = base_slug
+            counter = 1
+            while Section.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
     
 
     
@@ -38,3 +55,12 @@ class Available_sections(models.Model):
     class Meta:
          unique_together = ("student", "section")
 
+# To deal with the accessed course objects 
+# 1. student1 = Section.objects.all().first()
+# 2. course1 = Course.objects.all().last()
+
+# 3. Section.objects.create(section_name ="section 1", section_description="section to play twice to test slug", belong_to_course = course1)
+
+# 4. sections_with_access = student1.available_sections.all()
+# 5. sections_with_access.first().section                       -> here it returns section object that is related to the student (First one)
+# 6. sections_with_access.first().section.section_name          -> here we get the name for the section name
--- static/courses/js/courses.js
+++ static/courses/js/courses.js
@@ -46,50 +46,92 @@ buttonElement4.addEventListener('click', function () {
 
 function showgraphs()
 {
-    document.getElementById('teacher-dashboard-main-content').innerHTML = ` <div style="text-align:center;"> Student Analytics </div> <div id="chart-container" style="border: 1px solid black; padding: 0.3em; width:fit-content;"></div>
-     <br> <canvas width="100" height="25" id="completionChart" ></canvas> `;
+    document.getElementById('teacher-dashboard-main-content').innerHTML = ` 
+  <div style="background: linear-gradient(rgba(236, 240, 243, 0.4), rgba(209,215,227, 0.4))">
+     <div style="padding: 1em;"></div>
+     <h3 style="text-align:center;">
+     Student Analytics </h3> 
+     <div class="row" style="margin:0px; padding: 2em 1em; border-radius: 1em;">
 
-    // D3.js for graphs
+     <div class="col-lg-3 col-graph" >
+       <div style="width: 15em; height: 17.5em; border: 1px solid grey; padding: 0.5em; box-shadow: 1em 1em 2em rgb(200, 200, 200);
+        background-color: white; border-radius: 1em;"> 
+          <div style="border-bottom: 1px solid grey; padding-bottom:5px;"> Students Capabilities </div> 
+          <canvas id="studentChart" width="100" height="100"></canvas> 
+       </div>
+     </div> 
+     
+     <div class="col-lg-6 mx-auto col-graph" >
+     <div style="width: 25em; height: 17.5em; border: 1px solid grey; padding: 0.5em; box-shadow: 1em 1em 2em rgb(200, 200, 200);
+     background-color: white; border-radius: 1em;"> 
+     <div style="border-bottom: 1px solid grey; padding-bottom:5px;"> Course completed by students </div>
+     <canvas width="25" height="15" id="completionChart" ></canvas> </div>
+     </div>
 
-const data = [10, 20, 30, 40, 190];
-const labels = ["A", "B", "C", "D", "E"]; // Labels for the bars
+    <div class="col-lg-3 col-graph" >
+    <div style="width: 15em; height: 17.5em; border: 1px solid grey; padding: 0.5em; box-shadow: 1em 1em 2em rgb(200, 200, 200);
+    background-color: white; border-radius: 1em;"> 
+    <div style="border-bottom: 1px solid grey; padding-bottom:5px;"> Different student levels </div> 
+     <canvas id="studentChart3" width="100" height="100"></canvas> </div>
+     </div>
+      </div>  <!-- End of the row -->
 
-// Set up chart dimensions
-const width = 400;
-const height = 200;
-const barWidth = width / data.length;
+      <!-- new row begins -->
+      <div class="row" style="margin: 0px; justify-content:space-evenly; padding:0em 2em; align-items:center;">
+         
+         <div class="col-lg-7 col-graph">
+             <div style="width: 32em; height: 17.5em; border: 1px solid grey; padding: 0.5em; box-shadow: 1em 1em 2em rgb(200, 200, 200);
+              background-color: white; border-radius: 1em;"> 
+                     <div style="border-bottom: 1px solid grey; padding-bottom:5px;">
+                       Number of students per Course </div>
+                     <canvas width="30" height="14" id="Students-by-Courses" ></canvas>        
+             </div>
+         </div>
 
-// Create an SVG element for the chart
-const svg = d3.select("#chart-container")
-.append("svg")
-.attr("width", width)
-.attr("height", height);
+         <div class="col-lg-5 col-graph">
+            <div style="width: 25em; height: 17.5em; border: 1px solid grey; padding: 0.5em; box-shadow: 1em 1em 2em rgb(200, 200, 200);
+             background-color: white; border-radius: 1em;">
+                <div style="border-bottom: 1px solid grey; padding-bottom:5px;">
+                  Test Scores </div>
+            </div>
+         </div>
 
-// Create and append rectangles for the bars
-svg.selectAll("rect")
-.data(data)
-.enter()
-.append("rect")
-.attr("x", (d, i) => i * barWidth)
-.attr("y", (d) => height - d)
-.attr("width", barWidth - 1)
-.attr("height", (d) => d)
-.attr("fill", "steelblue");
-
-// Add labels for each bar
-svg.selectAll("text")
-.data(labels)
-.enter()
-.append("text")
-.text((d, i) => labels[i]) // Use the labels array for text
-.attr("x", (d, i) => i * barWidth + barWidth / 2) // Center the text
-.attr("y", (d) => height - d - 5) // Adjust vertical position
-.attr("text-anchor", "middle") // Center align the text
-.attr("fill", "white"); // Text color
+      </div>  <!-- end of new row -->
 
 
+  </div> `;
 
-// For chart.js
+// (1) - For [pie chart]-  chart.js
+const data3 = {
+  labels: ["Passed", "Failed"],
+  datasets: [
+    {
+      data: [75, 25], // You can adjust these values based on your data
+      backgroundColor: ["#36A2EB", "#FFCE56"], // Colors for each segment
+    },
+  ],
+};
+const ctx2 = document.getElementById("studentChart").getContext("2d");
+
+const options1 = {
+  title: {
+    display: true,
+    text: "Student Course Progress",
+    fontSize: 16,
+  },
+  responsive: true,
+};
+
+const pieChart = new Chart(ctx2, {
+  type: "pie",
+  data: data3,
+  options: options1,
+});
+//Ended here ....
+
+
+
+// (2) - For chart.js
 const completedCount = 75; // Number of students who completed the course
 const notCompletedCount = 25; // Number of students who did not complete the course
 
@@ -100,7 +142,7 @@ const ctx = document.getElementById("completionChart").getContext("2d");
 const data2 = {
   labels: ["Completed", "Not Completed"],
   datasets: [{
-    label: "Number of Students that have completed the course",
+    label: "Student Completed Course",
     data: [completedCount, notCompletedCount],
     backgroundColor: ["green", "red"],
   }]
@@ -118,6 +160,68 @@ const myChart = new Chart(ctx, {
     },
   },
 }); // End of chart.js graph
+
+
+
+// (3) - chart.js chart-3 "doughnut chart! "
+const ctx4 = document.getElementById("studentChart3").getContext("2d");
+
+const data4 = {
+  labels: ["Level 1", "Level 2", "Level 3", "Level 4"],
+  datasets: [
+    {
+      label: "Students by Level",
+      data: [10, 20, 30, 40],
+      backgroundColor: ["#FF0000", "#FF9900", "#FFFF00", "#00FF00"],
+    },
+  ],
+};
+
+const options3 = {
+  title: {
+    display: true,
+    text: "Number of Students by Level",
+  },
+};
+
+const chart = new Chart(ctx4, {
+  type: "doughnut",
+  data: data4,
+  options: options3,
+}); //End of the 3rd chart
+
+
+
+// (4) - Histogram chart of chart.js
+const data5 = {
+  labels: ["Course 1", "Course 2", "Course 3", "Course 4"],
+  datasets: [
+    {
+      label: "Student Distribution",
+      data: [45, 60, 80, 95], // Example scores for each difficulty
+      backgroundColor: ["#36A2EB", "#FFCE56", "#FF6384", "#4BC0C0"], // Colors for each bar
+    },
+  ],
+};
+
+const ctx5 = document.getElementById("Students-by-Courses").getContext("2d");
+
+const histogramChart = new Chart(ctx5, {
+  type: "bar",
+  data: data5,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        // max: 500, // Set the maximum value on the y-axis to 100%
+        title: {
+          display: true,
+          text: "Number of students",
+        },
+      },
+    },
+  },
+}); //End of the (4)th graph
 
 }
 
