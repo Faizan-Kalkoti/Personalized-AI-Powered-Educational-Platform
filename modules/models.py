a4131f5604dea5e7577fd2bf2e742574fe6d719e
Faizan <faizan.kalkoti@gmail.com>
2023-11-03 02:20:14 +0530

    added modules application

---
+++ modules/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.text import slug
+from accounts.models import Teacher, Student
+
+from django.utils import timezones
+
+# Create your models here.
